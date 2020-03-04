8fbe4814fa588f3b584da712180c2b115c568916
= <=>
2020-03-04 14:28:47 -0800

    Update dejumbler with readable outputs and text cleaning

--- Code/fumple.py
+++ Code/fumple.py
@@ -43,7 +43,6 @@ def _multi_word_dejumbler(jumble_letters, word_lengths, cur_words, out_list, d, 
     for combo, compliment in c.combo_compliments(jumble_letters, word_lengths[_ndx]):
         valid_words = dejumbler(combo, d)
         if len(compliment) == 0:
-                print(cur_words)
                 let_list = list(cur_words)
                 longth = len(cur_words)
                 str_comp = ''.join(jumble_letters)
@@ -51,7 +50,7 @@ def _multi_word_dejumbler(jumble_letters, word_lengths, cur_words, out_list, d, 
                 out_list[len(word_lengths) - longth].append(let_list)
 
         if len(valid_words) == 0:
-            print(cur_words)
+            pass
             
         for valid_word in valid_words:
             new_words = list(cur_words)
@@ -59,7 +58,7 @@ def _multi_word_dejumbler(jumble_letters, word_lengths, cur_words, out_list, d, 
             _multi_word_dejumbler(compliment, word_lengths, new_words, out_list, d, _ndx)
 
 def dictionary_melter():
-    f = open('big_dict.txt')
+    f = open('medium_dict.txt')
     table = Hashtable()
 
     index = -1
@@ -78,10 +77,10 @@ def dictionary_melter():
 
 def dictionary_melter_std():
     """
-    I know using the pythomn dict is kind of cheating, but I've 
+    I know using the python dict is kind of cheating, but I've 
     had enough of serialization ever since I got into Unity
     """
-    f = open('big_dict.txt')
+    f = open('medium_dict.txt')
     table = {}
 
     index = -1
@@ -140,20 +139,55 @@ def meltionary_reader():
     f.close()
     return ds
 
-def jumble_runner(meltionary):
-     
-    primary_jumbles = [
-        ['tefon', '--o-o'],
-        ['sokik', 'oo-o-'],
-        ['niumem', '----o-'],
-        ['siconu', '---oo-']
-    ]
+def index_sort(list_in):
+    associator = []
+    retlist = [[], []]
+    for index, item in enumerate(list_in):
+        associator.append((item, index))
+    associator.sort(key=lambda tpl: tpl[0])
+    for tpl in associator:
+        retlist[0].append(tpl[0])
+        retlist[1].append(tpl[1])
+    return retlist
+
+def desort(list_items, list_ndxes):
+    retlist = [None] * len(list_ndxes)
+    if len(list_items) == len(list_ndxes):
+        for index_in, index_out in enumerate(list_ndxes):
+            retlist[index_out] = list_items[index_in]
+    else:
+        for index_in, index_out in enumerate(list_ndxes):
+            if index_in == len(list_items) - 1:
+                index_out = len(retlist)
+                retlist.append(list_items[index_in])
+                for index, space in enumerate(retlist):
+                    if space == None:
+                        retlist[index] = '_'
+            retlist[index_out] = list_items[index_in]
+
+    return retlist
+
+def jumble_runner(meltionary, primary_jumbles=None, final_join=' '):
+    """
+    How do you spell "clodgy"?
+    """
+    if primary_jumbles is None:
+        primary_jumbles = [
+            ['tefon', '--o-o'],
+            ['sokik', 'oo-o-'],
+            ['niumem', '----o-'],
+            ['siconu', '---oo-']
+        ]
 
     answers = []
     for jumble in primary_jumbles:
         answers.append(dejumbler(jumble[0], meltionary))
 
-    secondary_word_lengths = [6, 2]
+    secondary_word_lengths = [2, 6]
+    sort_indices = index_sort(secondary_word_lengths)
+    secondary_word_lengths = sort_indices[0]
+
+    word_lengths = sorted(secondary_word_lengths)
     secondary_letters = ''
     for ndx, jumble in enumerate(primary_jumbles):
         for index, letter in enumerate(jumble[1]):
@@ -162,11 +196,35 @@ def jumble_runner(meltionary):
 
     secondary_answers_list = multi_dejumble(secondary_letters, secondary_word_lengths, meltionary)
 
+    print()
+    for index, answer in enumerate(answers):
+        print('Jumble ' + str(index) + ' is: ' + ' '.join(answer))
     
-    print()
-    print(answers)
-    print()
-    print(secondary_answers_list[0])
+
+    for ndx, _ in enumerate(secondary_answers_list):
+        for index, ls in enumerate(secondary_answers_list[ndx]):
+            secondary_answers_list[ndx][index] = tuple(desort(ls, sort_indices[1]))
+
+        secondary_answers_list[ndx] = list(dict.fromkeys(secondary_answers_list[ndx]))
+
+    ndx = 0
+
+    for index, ls in enumerate(secondary_answers_list):
+        if len(ls) != 0:
+            ndx = index
+            break
+
+    print()  
+    if ndx == 0:
+        print('Final jumble: ', end='')
+    elif ndx == 0:
+        print('I only have guesses for the longest word in the final jumble, \nI\'ive included the remaining letters for your convenience: ', end='')
+    else:
+        print('I only have guesses for the longest ' + str(ndx) + ' words in the final jumble, \nI\'ive included the remaining letters for your convenience: ', end='')
+    for answer in secondary_answers_list[ndx]:
+        print(final_join.join(answer))
+
+    #print(secondary_answers_list[0])
     print()
 
     return (answers, secondary_answers_list)
@@ -201,10 +259,10 @@ if __name__ == "__main__":
 
     #cProfile.run('dictionary_melter()')
     d = dictionary_melter_std()
-    #meltionary_writer(d)
-    #d = meltionary_reader()
+    meltionary_writer(d)
+    d = meltionary_reader()
     #meltionary_writer(d)
     #cProfile.run('jumble_runner(d)')
-    jumble_runner(d)
+    jumble_runner(d, final_join='-')
     #print(dejumbler('short', d))  # sardine
 
